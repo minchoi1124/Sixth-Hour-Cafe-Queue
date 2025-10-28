667bf7a12d798e3963f684ffe1c34e42f688849b
Min Hyuk Choi <minhc3@uci.edu>
2025-10-28 19:39:28 +0000

    chore: update project behavior

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "REDACTED_FIREBASE_PROJECT_ID",
+  "appId": "REDACTED_FIREBASE_APP_ID",
+  "apiKey": "REDACTED_FIREBASE_API_KEY",
+  "authDomain": "REDACTED_FIREBASE_PROJECT_ID.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "REDACTED_FIREBASE_SENDER_ID"
+};

1068c9d376728b7e57e8eb8443f073b8e1e5280f
Min Hyuk Choi <minhc3@uci.edu>
2025-10-28 19:39:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2882078798-8b293",
+  "appId": "1:1078360229257:web:624291c176187702c00d58",
+  "apiKey": "AIzaSyDnTFAlKlSTDCzPlATH0P7N-9BP0W9ZkOg",
+  "authDomain": "studio-2882078798-8b293.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1078360229257"
+};
